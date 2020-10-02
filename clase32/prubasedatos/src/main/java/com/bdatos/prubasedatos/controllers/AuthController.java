0b944c64a05f32c1129be402aa56590a5c51e596
Andres Castellanos Pallares <andresrobanesky@gmail.com>
2020-10-01 20:15:39 -0500

    se añade clase 32

---
+++ clase32/prubasedatos/src/main/java/com/bdatos/prubasedatos/controllers/AuthController.java
@@ -0,0 +1,52 @@
+package com.bdatos.prubasedatos.controllers;
+
+import com.bdatos.prubasedatos.repositories.UserRepository;
+import com.bdatos.prubasedatos.requests.LoginRequest;
+import com.bdatos.prubasedatos.responses.JwtResponse;
+import com.bdatos.prubasedatos.security.jwt.JwtUtils;
+import com.bdatos.prubasedatos.servicesimpl.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+
+//@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+	@Autowired
+	AuthenticationManager authenticationManager;
+
+	@Autowired
+	UserRepository userRepository;
+
+
+	@Autowired
+	PasswordEncoder encoder;
+
+	@Autowired
+	JwtUtils jwtUtils;
+
+	@PostMapping("/signin")
+	public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
+
+		Authentication authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		String jwt = jwtUtils.generateJwtToken(authentication);
+		
+		UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+		return ResponseEntity.ok(new JwtResponse(jwt,
+												 userDetails.getId(), 
+												 userDetails.getUsername(), 
+												 userDetails.getEmail()
+												 ));
+	}
+}
